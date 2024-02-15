e6bd7a6892475a7eb7069af781a52e1234869fc5
Gra1gkolodziejczyk <graig.kolodziejczyk@ynov.com>
2024-02-15 14:48:51 +0100

    all files

--- src/App.vue
+++ src/App.vue
@@ -1,85 +1,134 @@
 <script setup lang="ts">
-import { RouterLink, RouterView } from 'vue-router'
-import HelloWorld from './components/HelloWorld.vue'
+import { ref, onMounted, watch } from "vue";
+
+interface Todo {
+  todo: string;
+  done: boolean;
+}
+
+const todos = ref<Todo[]>([]);
+const text = ref<string>("");
+const inputError = ref<boolean>(false);
+
+function addTodo() {
+  if (text.value.trim() === "") {
+    inputError.value = true;
+    return;
+  } else {
+    inputError.value = false;
+  }
+
+  todos.value.unshift({
+    todo: text.value,
+    done: false,
+  });
+
+  text.value = "";
+}
+
+function deleteTodo(todo: Todo) {
+  todos.value = todos.value.filter((x) => x !== todo);
+}
+
+watch(
+  todos,
+  (newTodoValue) => {
+    localStorage.setItem("todos", JSON.stringify(newTodoValue));
+  },
+  { deep: true }
+);
+
+onMounted(() => {
+  const savedTodos = localStorage.getItem("todos");
+  if (savedTodos) {
+    todos.value = JSON.parse(savedTodos) as Todo[];
+  }
+});
 </script>
 
 <template>
-  <header>
-    <img alt="Vue logo" class="logo" src="@/assets/logo.svg" width="125" height="125" />
+  <main class="app">
+    <section class="greeting">
+    </section>
 
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
+    <div class="input-section">
+      <section class="create-todo">
+        <h3>Ajouter une tâche</h3>
+        <form class="form" @submit.prevent="addTodo">
+          <input
+            type="text"
+            placeholder="écrit la tâche à faire"
+            v-model="text"
+          />
 
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+          <div class="input-add">
+            <input type="submit" value="Ajouter" />
+          </div>
+        </form>
+        <span v-if="inputError" class="error-message">Veuillez entrer une tâche avant d'ajouter</span>
+      </section>
     </div>
-  </header>
+    <div class="todo-section">
+      <section class="todo-list">
+        <h2 v-show="todos.length === 0">Pas encore de tâches...</h2>
 
-  <RouterView />
+        <div class="list">
+          <div :key="todo.todo" v-for="todo in todos" :class="`todo-item ${todo.done && 'done'}`">
+            <label class="label">
+              <input type="checkbox" v-model="todo.done" />
+            </label>
+
+            <div class="todo-content">
+              <input type="text" v-model="todo.todo"/>
+            </div>
+
+            <div class="action-delete">
+              <button class="delete" @click="deleteTodo(todo)">Supprimer</button>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+  </main>
 </template>
 
-<style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+<style>
+.app {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: column;
+  margin-top: 10%;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
+.todo-item {
+  margin-top: 30px;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
 }
 
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
+.error-message {
+  color: red;
 }
 
-nav a.router-link-exact-active {
-  color: var(--color-text);
+.form {
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  flex-direction: row;
 }
 
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
+.input-add {
+  padding-left: 10px;
 }
 
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
+.label {
+  padding-right: 10px;
 }
 
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+.action-delete {
+  padding-left: 10px;
 }
 </style>
